23b5cbd83dbd1eff6dbdf2f88f9e105f4bce1447
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-03-10 18:32:37 +0100

    Added comments and repaired middle element

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,15 @@
 def binary_search(list, item):
-    low = 0
-    high = len(list)-1
+    low = 0             # Dolna granica
+    high = len(list)-1  # Górna granica
 
     while low <= high:
-        mid = (low + high)
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
+        mid = (low + high)//2  # Środkowy element
+        guess = list[mid]      # Zgadywany element
+        if guess == item:      # Znaleziono element
+            return mid      
+        if guess > item:       # Element za duży
+            high = mid - 1          
+        else:                  # Element za mały
             low = mid + 1
-    return None
+    return None                # Nie ma takiego elementu
        
